bcec34cdbfc73628e1aa15dd90650e49cf900262
ogaluechibueze <ogaluechibueze@yahoo.com>
2024-11-24 19:04:36 +0100

    first commit

---
+++ src/ReactUI.jsx
@@ -0,0 +1,19 @@
+import CameraController from "./reactComponents/CameraController";
+import SocialModal from "./reactComponents/SocialModal";
+import EmailModal from "./reactComponents/EmailModal";
+import ProjectModal from "./reactComponents/ProjectModal";
+
+export default function ReactUI() {
+  return (
+    <>
+       <div className="controls-message">
+      <p className="">Tap/Click around to move</p>
+      <img src="/logos/genius.png" alt="GeniusWorld" width="200px" height="200px"/>
+      </div>
+      <CameraController />
+      <SocialModal />
+      <EmailModal />
+      <ProjectModal />
+    </>
+  );
+}
---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const PALETTE = {
+    color1: "#272946",
+    color2: "#aaea6c",
+    color3: "#e7ffee",
+    color4: "#FF0000",
+  };
+  
+  export const DIAGONAL_FACTOR = 1 / Math.sqrt(2);
+  export const ZOOM_MAX_BOUND = 2;
+  export const ZOOM_MIN_BOUND = 0.2;
+  
